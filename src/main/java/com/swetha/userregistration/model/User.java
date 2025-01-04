2324e37531ff0789373948ac56fc70fa1329427d
swethapriya <sswethapriya2002@gmail.com>
2025-01-04 15:28:53 +0530

    models

---
+++ src/main/java/com/swetha/userregistration/model/User.java
@@ -0,0 +1,31 @@
+package com.swetha.userregistration.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a user in the system.
+ */
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;    
+}
