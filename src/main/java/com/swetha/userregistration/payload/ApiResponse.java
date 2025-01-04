76950ecd6d26d536badcdbab284112ae0617e7f7
swethapriya <sswethapriya2002@gmail.com>
2025-01-04 15:29:37 +0530

    payloads

---
+++ src/main/java/com/swetha/userregistration/payload/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.swetha.userregistration.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+    private boolean success;
+    private Integer responseCode;
+}
